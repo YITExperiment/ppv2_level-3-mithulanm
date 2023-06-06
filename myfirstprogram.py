b44359929207965aecec35b4f75fb8ee5571174d
mithulanm <134401634+mithulanm@users.noreply.github.com>
2023-06-06 07:03:28 -0700

    Add files via upload

---
+++ myfirstprogram.py
@@ -0,0 +1,15 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/MITHULAN/Desktop/myfirstprogram.py =============
+Hello, World!
+What is your name?Rakshala
+Hello Rakshala
+>>> 
+>>> weight=50
+>>> print(weight)
+50
+>>> 
+>>> height=130
+>>> print(height)
+130
