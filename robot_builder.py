f07c3588e3621d0d710b5becac4bc2f809bb118b
mithulanm <134401634+mithulanm@users.noreply.github.com>
2023-06-16 06:20:21 -0700

    Add files via upload

--- robot_builder.py
+++ robot_builder.py
@@ -55,13 +55,14 @@ rectangle(80,50,'yellow')
 #eyes
 t.goto(-60, 160)
 rectangle(30,10,'white')
-t.goto(-55,155)
+t.goto(-60,160)
 rectangle(5,5,'black')
-t.goto(-40,155)
+t.goto(-45,155)
 rectangle(5,5,'black')
 
 #mouth
 t.goto(-65,135)
+t.right(5)
 rectangle(40,5,'black')
 
 t.hideturtle()
